9d528be37dc3fd3cb002a539166e5c364a2ac891
ana <anamariaguzmanh1996@gmail.com>
2024-09-17 20:09:45 -0500

    my first compoment on talento tech

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,16 @@
+export const MyFirstComponent = () => {
+  return (
+    <div>
+        MyFirstComponent
+        <h1>Lista</h1>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+
+
+        </div>
+  )
+}
